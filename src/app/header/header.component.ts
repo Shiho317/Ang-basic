7033c5e1c130edf46e20421cb2b1a70298b90b1a
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-19 00:10:14 -0800

    added navbar func

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public arrowIcon: string = 'fa-angle-down';
+  public dropNav: string = 'drop-up';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleCompany(): void{
+    if(this.arrowIcon === 'fa-angle-down'){
+      this.arrowIcon = 'fa-angle-up';
+      this.dropNav = 'drop-down';
+    }else{
+      this.arrowIcon = 'fa-angle-down';
+      this.dropNav = 'drop-up';
+    };
+    
+  }
+
+  
 
 }
